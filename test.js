675b7ac7ff6760f94ab18bc01ed8e1c1c4870681
Fatma Siam <fatma.o.siam@gmail.com>
2019-01-15 14:40:02 +0200

    Make test To AddtoDo function #2

--- test.js
+++ test.js
@@ -7,30 +7,17 @@ test('Example test', function(t) {
   t.pass();
   t.end();
 });
-test('todoarray',function (t){
+test('Add -todoarray',function (t){
 
-  const actual =logic.addTodo(todos,newtodo);
-  const expected= [{id:0,description:"fatma",done:false},{id:1,description:"angham",done:false}]
+  const actual =logic.addTodo(todos,"Sama");
+  const expected= [{id:0,description:"fatma",done:false},{id:1,description:"Sama",done:false}]
 
-  t.deepEqual(actual,expected,'Pass');
+  t.deepEqual(actual,expected,'Add new to do ');
   t.end();
 
 });
 
 
 
-var nottodos=[{id:0,description:"fatma",done:false}];
-var newnottodo=[{id:1,description:"angham",done:false}];
-test('another Example test', function(q) {
-  q.pass();
-  q.end();
-});
-test('DeleteList',function (q){
 
-  const actual =logic.deleteTodo(nottodos,newnottodo);
-  const expected= [{id:1,description:"Anies",done:true,{id:2,description:"Ahmad",done:true}]
 
-  t.deepEqual(actual,expected,'Pass');
-  t.end();
-
-});
